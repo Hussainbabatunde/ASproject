bd445f373165997962fb537cfddefa3dacae2a1e
Hussain Babatunde <tudemine@gmail.com>
2023-11-08 03:59:50 -0800

    notification works

--- src/Components/Notification/Notification.js
+++ src/Components/Notification/Notification.js
@@ -1,41 +1,51 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import Pusher from 'pusher-js';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+
+const client = axios.create({baseURL: 'https://ko.bcodestech.com/api/'})
+
+const getNotification = () => 
+{
+  let token = localStorage.getItem('token')
+    // return axios.get(`http://localhost:8000/api/get-offer-notification/${heroId}`) 
+    const options = { url: 'offer-notification', method: 'GET'   }
+    client.defaults.headers.common.Authorization = `Bearer ${token}`
+    const onSuccess = (response) => response
+    const onError = (error) => {
+        return error
+    }
+    return client(options).then(onSuccess).catch(onError)
+}
 
 const Notification = () =>{
     const userId = useSelector((state)=> state?.reducer?.LoginSlice?.logindata?.data?.user?.id)
     console.log('host name ',window.location.hostname)
 
-    useEffect(() => {
-        const pusher = new Pusher('myKey', {
-          cluster: 'eu2',
-          wsHost: 'localhost',
-          wssHost: 'localhost',
-          wsPort: 6001,
-          disableStats: true,
-          encrypted: true, // Use wss for secure WebSocket connection
-        });
+    const { isLoading, data, isError, error, refetch, onSuccess, onError  } = useQuery('recent-offer', getNotification, { 
+      cacheTime: 0, 
+      refetchOnMount: true, 
+      refreshOnWindowFocus: true,  
+      refreshInterval: 300,
+      refreshIntervalInBackground: true,
+      enable: true,
+    })
 
-        const channel = pusher.subscribe(`offer.${userId}`);
-
-    channel.bind('OfferNotificationEvent', (data) => {
-      // Handle the incoming event data here
-      console.log('Received event:', data);
-    });
-
-    return () => {
-        // Clean up the Pusher connection when the component unmounts
-        pusher.unsubscribe(`offer.${userId}`);
-        pusher.disconnect();
-      };
-    }, []);
+    // console.log("***********************8")
+    //   console.log('notification 2 ',data)
+    //   console.log("***********************8")
+    
     return(
         <div className='my-5 min-h-[50vh] bg-green'>
             <div className='w-[60%] bg-white mx-auto'>
-                <div className='flex items-center'>
-            <img src={require('../../assets/imgRecipient.png')}  className='useTable_ImageRecipient' />
-            <p className='font-bold'>Scout commented on your page.</p>
+                {data?.data?.toReversed()?.map((each, index)=>(
+                <div key={index} className='flex items-center mt-2 mb-2'>
+            <img src={each?.offer?.sender?.profile_pics}  className='useTable_ImageRecipient' />
+            <p className='font-bold'>{each?.offer?.sender?.firstname} {each?.offer?.sender?.surname} pitched an offer.</p>
             </div>
+            ))}
             </div>
         </div>
     )
